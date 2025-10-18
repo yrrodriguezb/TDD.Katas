b6de89be14d2408d3281d536aa8d5d0891ad9c17
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-10-18 14:10:57 -0500

    🔴 Si_SeIngresaLaLetraB_Debe_RetornarVerdaderoParaInidicarQueEsUnPalindromo

--- Palindromos.Tests/Palindromos.Tests.cs
+++ Palindromos.Tests/Palindromos.Tests.cs
@@ -14,6 +14,12 @@ public class PalindromosTests
         EsPalindromo("A").Should().Be(true);
     }
 
+    [Fact]
+    public void Si_SeIngresaLaLetraB_Debe_RetornarVerdaderoParaInidicarQueEsUnPalindromo()
+    {
+        EsPalindromo("B").Should().Be(true);
+    }
+
     private static bool EsPalindromo(string str)
     {
         return str == "A";
