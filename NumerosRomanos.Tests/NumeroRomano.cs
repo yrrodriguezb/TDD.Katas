801f22e7c29c84c1f8f3134b7484393aeb278154
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-03 12:00:26 -0500

    🔵 Numero Romano - Si_Ingreso2_Debe_RetornarII

--- NumerosRomanos.Tests/NumeroRomano.cs
+++ NumerosRomanos.Tests/NumeroRomano.cs
@@ -3,16 +3,23 @@ namespace NumerosRomanos.Tests;
 public class NumeroRomano
 {
     private readonly int _numero;
+    private readonly string _romano = string.Empty;
 
     public NumeroRomano(int numero)
     {
         _numero = numero;
+        _romano = ConvertirARomano();
+    }
+
+    private string ConvertirARomano()
+    {
+        if (_numero == 1) return "I";
+        if (_numero == 2) return "II";
+        return string.Empty;
     }
 
     public override string ToString()
     {
-        if (_numero == 1) return "I";
-
-        return "II";
+        return _romano;
     }
 }
