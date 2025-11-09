c477f8e52e1457bc4af97efc76728440c1728dfd
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-09 13:40:50 -0500

    🔵 Numero Romano - Si_IngresoElNumero7_Debe_RetornarVII

--- NumerosRomanos.Tests/NumeroRomano.Tests.cs
+++ NumerosRomanos.Tests/NumeroRomano.Tests.cs
@@ -8,42 +8,15 @@ public class NumerosRomanosTests
     [InlineData(1, "I")]
     [InlineData(2, "II")]
     [InlineData(3, "III")]
-    public void Si_IngresoUnNumeroEntre1y3_Debe_RetonarElRomanoCorrespondiente(int numero, string valorEsperado)
+    [InlineData(4, "IV")]
+    [InlineData(5, "V")]
+    [InlineData(6, "VI")]
+    [InlineData(7, "VII")]
+    public void Si_IngresoUnNumeroEntre1y7_Debe_RetonarElRomanoCorrespondiente(int numero, string valorEsperado)
     {
         var resultado = new NumeroRomano(numero).ToString();
 
         resultado.Should().Be(valorEsperado);
     }
 
-    [Fact]
-    public void Si_IngresoElNumero4_Debe_RetornarIV()
-    {
-        var resultado = new NumeroRomano(4).ToString();
-
-        resultado.Should().Be("IV");
-    }
-
-    [Fact]
-    public void Si_IngresoElNumero5_Debe_RetornarV()
-    {
-        var resultado = new NumeroRomano(5).ToString();
-
-        resultado.Should().Be("V");
-    }
-
-    [Fact]
-    public void Si_IngresoElNumero6_Debe_RetornarVI()
-    {
-        var resultado = new NumeroRomano(6).ToString();
-
-        resultado.Should().Be("VI");
-    }
-    
-    [Fact]
-    public void Si_IngresoElNumero7_Debe_RetornarVII()
-    {
-        var resultado = new NumeroRomano(7).ToString();
-
-        resultado.Should().Be("VII");
-    }
 }
