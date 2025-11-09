6bd18fc0deea9b376482bb8864128f18bb9a59d6
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-09 14:47:04 -0500

    🟢 Numero Romano - Si_IngresoElNumero500_Debe_RetonarD

--- NumerosRomanos.Tests/NumeroRomano.cs
+++ NumerosRomanos.Tests/NumeroRomano.cs
@@ -16,7 +16,8 @@ public class NumeroRomano
         {50, "L"},
         {90, "XC"},
         {100, "C"},
-        {400, "CD"}
+        {400, "CD"},
+        {500, "D"}
     };
 
     public NumeroRomano(int numero)
