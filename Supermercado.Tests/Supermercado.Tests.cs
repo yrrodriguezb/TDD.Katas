ae50743b78b2df991feadca4ab34184ba85e9c6e
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-09 22:24:33 -0500

    🔴  Supermercado - Si_SeInicializaUnSupermercado_Debe_RetornarUnRecibo

---
+++ Supermercado.Tests/Supermercado.Tests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+
+namespace Supermercado.Tests;
+
+
+public class SupermercadoTests
+{
+    [Fact]
+    public void Si_SeInicializaUnSupermercado_Debe_RetornarUnRecibo()
+    {
+        var supermercado = new SuperMarket();
+
+        Recibo recibo = supermercado.GenerarRecibo(new List<ArticuloCarrito>());
+
+        recibo.Should().NotBeNull();
+    }
+}
+
+public class ArticuloCarrito
+{
+}
+
+public class Recibo
+{
+}
+
+public class SuperMarket
+{
+    public Recibo GenerarRecibo(List<ArticuloCarrito> list)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Supermercado.Tests/TestList.md
@@ -0,0 +1,6 @@
+# Supermercado
+
+1. Debe existir un carrito de compras
+2. Debe existir una lista de productos
+3. Debe tener funcionalidad para ofrecer promociones
+4. Debe generar un recibo con el detalle de pago
