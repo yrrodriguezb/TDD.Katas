7a05a9b5468d0c5d37122eab05f4c310bd731712
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-15 17:15:30 -0500

    🟢 Supermercado - Si_GeneroUnReciboConElProductoLeche_Debe_TenerUnArticuloUnTotalDe1000

--- Supermercado.Tests/Recibo.cs
+++ Supermercado.Tests/Recibo.cs
@@ -1,8 +1,26 @@
-﻿namespace Supermercado.Tests;
+﻿using System.Collections.ObjectModel;
+
+namespace Supermercado.Tests;
 
 public class Recibo
 {
-    public List<ArticuloCarrito> Articulos { get; set; } = [];
-    public decimal Total { get; }
-    public decimal SubTotal { get; }
+    private List<ArticuloCarrito> _articulos = new();
+    private int _total = 0;
+    private int _subTotal = 0;
+
+    public Recibo()
+    {
+        
+    }
+    
+    public Recibo(List<ArticuloCarrito> articulos)
+    {
+        _articulos  = articulos;
+        _total = 1000;
+        _subTotal = 1000;
+    }
+
+    public ReadOnlyCollection<ArticuloCarrito> Articulos => _articulos.AsReadOnly();
+    public decimal Total => _total;
+    public decimal SubTotal => _subTotal;
 }
--- Supermercado.Tests/SuperMarket.cs
+++ Supermercado.Tests/SuperMarket.cs
@@ -15,6 +15,11 @@ public class SuperMarket
     }
     public Recibo GenerarRecibo(List<ArticuloCarrito> articulos)
     {
+        if (articulos.Count > 0)
+        {
+            return new Recibo(articulos);
+        }
+        
         return new Recibo();
     }
 
