2a69689c7c69b62ca0f46dec72f6b280865157ca
Yeison Rodriguez <yrrodriguezb@gmail.com>
2025-11-16 13:31:06 -0500

    🔴  Wrap - Si_SeIngresaUnTextoVacioYUnValorDeColumna1_Debe_RetornarUnTextoVacio

---
+++ WordWrap.Test/WordWrapTests.cs
@@ -0,0 +1,19 @@
+﻿using AwesomeAssertions;
+
+namespace WordWrap.Test;
+
+public class WordWrapTests
+{
+    [Fact]
+    public void Si_SeIngresaUnTextoVacioYUnValorDeColumna1_Debe_RetornarUnTextoVacio()
+    {
+        var resultado = Wrap("", 1);
+       
+       resultado.Should().Be("");
+    }
+
+    private object Wrap(string empty, int i)
+    {
+        throw new NotImplementedException();
+    }
+}
